d6eb1e4549067a3f01c61444ee89d1676caaaa77
edi <draghici.edi@gmail.com>
2025-08-06 16:06:09 +0300

    Normalize line endings

---
+++ src/test/java/com/example/ice_cream_shop/IceCreamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ice_cream_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceCreamShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
