d583ed4135a39b6acb78e478348b9a18e820bb8c
edi <draghici.edi@gmail.com>
2025-08-06 15:54:58 +0300

    feat: implement repository layer

---
+++ src/main/java/com/example/icecreamshop/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.example.icecreamshop.repository;
+
+import com.example.icecreamshop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderId(String orderId);
+} 
